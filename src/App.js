c3cf6441b15cb6536a3a362e0edf9f24c4c33265
Techoplite <oriccimirko@gmail.com>
2020-08-31 14:00:15 +0100

    Install node-sass/ Iniltialise styling

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import React from 'react';
-import './App.css';
+import './style.scss';
 
 function App() {
   return (
     <div className="App">
-      
+      <div className="div-1">
+        <h1>Front-end Developer</h1>
+        <code>Hello world, I am Mirko...</code>
+        <div className="circle"></div>
+      </div>
+      <div className="div-2"></div>
+      <div className="div-3"></div>
     </div>
   );
 }
