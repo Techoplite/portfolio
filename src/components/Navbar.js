934f41923028fefa8ac0b71df159c69bfc0dba70
Techoplite <oriccimirko@gmail.com>
2020-09-04 09:10:46 +0100

    Add hamburger animation

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,18 +2,19 @@ import React, { useState } from 'react'
 
 const Navbar = () => {
     const [navLinks, setNavLinks] = useState("hidden");
+    const [hamburger, setHamburger] = useState("closed");
     const handleOnClick = () => {
-
         navLinks === "hidden" ? setNavLinks("visible") : setNavLinks("hidden")
+        hamburger === "closed" ? setHamburger("open") : setHamburger("closed")
     }
 
     return (
         <div className="navbar">
             <div className="hamburger-wrapper" onClick={handleOnClick}>
                 <div className="hamburger">
-                    <div className="line"></div>
-                    <div className="line"></div>
-                    <div className="line"></div>
+                    <div className={`line ${hamburger}`} id="top-line"></div>
+                    <div className={`line ${hamburger}`} id="middle-line"></div>
+                    <div className={`line ${hamburger}`} id="bottom-line"></div>
                 </div>
             </div>
             <div className={`nav-links ${navLinks}`}>
