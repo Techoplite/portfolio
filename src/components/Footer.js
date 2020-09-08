11974a13cb4dfd5c70e51daa4dfdd2eb404b3656
Techoplite <oriccimirko@gmail.com>
2020-09-08 13:01:14 +0100

    Add scroll to top button

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,8 +1,19 @@
 import React from 'react'
+import { Link } from 'react-scroll'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faAngleDoubleUp } from '@fortawesome/free-solid-svg-icons'
+
+
 
 const Footer = () => {
     return (
         <section id="footer">
+            <Link
+                to="home"
+                smooth="true"
+                duration={500} >
+                <div className="scroll-to-top"><FontAwesomeIcon icon={faAngleDoubleUp} /></div>
+            </Link>
             Mirko Oricci ©2020
         </section>
     );
