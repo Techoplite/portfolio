25cb7564fe62b5159b6b15d05725a51c8ed639ab
Techoplite <oriccimirko@gmail.com>
2022-02-19 17:45:48 +0000

    Format saved file

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,19 +6,16 @@ import { faAngleDoubleUp } from '@fortawesome/free-solid-svg-icons'
 
 
 const Footer = () => {
-    return ( <
-        section id = "footer" >
-        <
-        Link to = "home"
-        smooth = "true"
-        duration = { 500 } >
-        <
-        div className = "scroll-to-top" > < FontAwesomeIcon icon = { faAngleDoubleUp }
-        /></div >
-        <
-        /Link>
-        Mirko Oricci© 2021 <
-        /section>
+    return (
+        <section id="footer">
+            <Link
+                to="home"
+                smooth="true"
+                duration={500} >
+                <div className="scroll-to-top"><FontAwesomeIcon icon={faAngleDoubleUp} /></div>
+            </Link>
+            Mirko Oricci ©2021
+        </section>
     );
 }
 
